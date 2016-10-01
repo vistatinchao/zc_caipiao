53c663c40e4bdde6bbc2e719334aaf859539ac95
vistatinchao <zouchao@zouchaodemacbook-pro.local>
2016-10-01 13:20:04 +0800

    Initial Commit

---
+++ zc_caipiao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zc_caipiao
+//
+//  Created by zouchao on 16/10/1.
+//  Copyright © 2016年 zouchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zc_caipiao/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  zc_caipiao
+//
+//  Created by zouchao on 16/10/1.
+//  Copyright © 2016年 zouchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
